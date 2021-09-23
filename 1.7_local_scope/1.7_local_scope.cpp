9bf244d88c41f3f7fb963f70ad74910db08f9492
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-09-23 17:29:11 +0900

    1.7 지역 범위

---
+++ 1.7_local_scope/1.7_local_scope.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+int main()
+{
+	int x(0);	// x = 0;
+
+	// {}: 영역 분리
+	{
+		int x = 2;
+
+		std::cout << x << " " << &x << std::endl;
+	}
+
+	{
+		int x = 1;
+
+		std::cout << x << " " << &x << std::endl;
+	}
+
+	std::cout << x << " " << &x << std::endl;
+
+	return 0;
+}
