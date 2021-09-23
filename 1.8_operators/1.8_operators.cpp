59d9766b7fd0e28b886b4b29d71130a50575ddb7
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-09-23 17:51:21 +0900

    1.8 연산자와의 첫 만남

---
+++ 1.8_operators/1.8_operators.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x = 2; // x is a variable, 2 is a literal
+
+    // binary operators (이항 연산자)
+    cout << x + 2 << endl;
+    cout << x - 2 << endl;
+    cout << x * 2 << endl;
+    cout << x / 2 << endl;
+
+    // unary operator (단항 연산자)
+    cout << -x << endl;     
+
+    // ternary operator (삼항 연산자)
+    cout << (x > 0 ? "yes" : "no") << endl;
+
+    return 0;
+}
