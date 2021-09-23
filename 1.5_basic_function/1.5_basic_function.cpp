3f9c3112598da997c12905a8c13d59aeaedce472
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-09-23 15:28:33 +0900

    1.5 함수와의 첫 만남

---
+++ 1.5_basic_function/1.5_basic_function.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int addTwoNumbers(int num_a, int num_b) {
+
+	int sum = num_a + num_b;
+
+	return sum;
+}
+
+void printHelloWorld() {
+
+	cout << "Hello World" << endl;
+
+	// return;	// 원한다면
+}
+
+int main() {
+
+	cout << 1 + 2 << endl;
+	cout << 3 + 4 << endl;
+	cout << 8 + 13 << endl;
+	printf("\n");
+
+	cout << addTwoNumbers(1, 2) << endl;
+	cout << addTwoNumbers(3, 4) << endl;
+	cout << addTwoNumbers(8, 13) << endl;
+	printf("\n");
+
+	printHelloWorld();
+	printHelloWorld();
+	printHelloWorld();
+	printHelloWorld();
+
+	return 0;
+}
