1a74ff3133503965cf13797807da3b1bc6c5eefb
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-09-24 10:31:47 +0900

    1.12 헤더가드가 필요한 이유

---
+++ 1.12_header_guard/1.12_header_guard.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+// 헤더가드가 없을 경우 같은 함수가 있을 경우 중복되어 build가 안될 수 있음
+#include "Header.h"
+#include "add.h"
+
+int main()
+{
+	doSomething();
+
+	return 0;
+}
---
+++ 1.12_header_guard/add.h
@@ -0,0 +1,12 @@
+#pragma once
+
+// header guard วฅมุ
+//#ifdef MY_ADD
+//#define MY_ADD
+
+int add(int a, int b) {
+
+	return a + b;
+}
+
+//#endif
