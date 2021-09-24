4ed409aacf32b641bba28c453c7fd7d439a549f6
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-09-24 10:41:39 +0900

    1.13 네임스페이스 (명칭공간)

---
+++ 1.13_namespace/1.13_namespace.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+namespace MySpace1 {
+	namespace InnerSpace {
+		int my_function() {
+			return 0;
+		}
+	}
+	int doSomething(int a, int b) {
+		return a + b;
+	}
+}
+
+namespace MySpace2 {
+	int doSomething(int a, int b) {
+		return a * b;
+	}
+}
+
+using namespace std;
+
+int main()
+{
+	{
+		using namespace MySpace1;
+		using namespace MySpace1::InnerSpace;
+
+		cout << doSomething(1, 2) << endl;
+		my_function();
+	}
+
+	cout << MySpace2::doSomething(3, 4) << endl;
+	cout << MySpace1::doSomething(1, 2) << endl;
+
+	return 0;
+}
