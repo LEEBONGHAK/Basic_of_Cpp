5a827bae2801cfb7c4d9c836d854a39c253bd0a6
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-09-24 12:59:34 +0900

    2.4 무치형 (보이드, void)

---
+++ 2.4_void/2.4_void.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+void my_function(void) {
+
+}
+
+int main()
+{
+	// void type: 메모리를 차지하지 않는 data type
+	// 포인터 다룰 때 casting하여 많이 사용됨
+
+	using namespace std;
+
+	//void my_void;
+	int i = 123;
+	float f = 123.456f;
+
+	void* my_void;
+
+	my_void = (void*)&i;
+	cout << my_void << endl;
+
+	my_void = (void*)&f;
+	cout << my_void << endl;
+
+	return 0;
+}
