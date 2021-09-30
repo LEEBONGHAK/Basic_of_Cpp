9cfe9d4b6111c4e1d64efe281e9e1bf85131027f
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-09-30 12:53:31 +0900

    3.2 산술 연산자 arithmetic operators

---
+++ 3.2_arithmetic_operators/3.2_arithmetic_operators.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <iomanip>	// setprecision()
+
+#define endl '\n'
+
+using namespace std;
+
+int main()
+{
+	int x = 1;
+	int y = -x;
+	cout << x << " " << y << endl;
+	printf("\n");
+
+	x = 6;
+	y = 2;
+	int z;
+
+	z = x + y;
+	cout << z << endl;
+	z = x - y;
+	cout << z << endl;
+	z = x * y;
+	cout << z << endl;
+	z = x / y;
+	cout << z << endl;
+	z = x % y;
+	cout << z << endl;
+	printf("\n");
+
+	cout << setprecision(3);
+	cout << x / y << endl;
+	cout << float(x) / y << endl;
+	cout << x / float(y) << endl;
+	cout << float(x) / float(y) << endl;
+	printf("\n");
+
+	z = x;
+	z += y;	// z = z + y
+	z -= y;	// z = z - y
+	z *= y;	// z = z * y
+	z /= y;	// z = z / y
+	z %= y;	// z = z % y
+
+	return 0;
+}
