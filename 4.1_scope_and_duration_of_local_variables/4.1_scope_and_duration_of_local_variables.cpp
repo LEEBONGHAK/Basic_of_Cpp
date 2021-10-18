9e5541a2707faa8854300cb701702ddbac2a6296
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-10-18 16:01:28 +0900

    4.1 지역 변수, 범위, 지속시간

---
+++ 4.1_scope_and_duration_of_local_variables/4.1_scope_and_duration_of_local_variables.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+#define endl '\n'
+using namespace std;
+
+namespace work1
+{
+	int a = 1;
+	void do1()
+	{
+		a += 3;
+	}
+}
+
+namespace work2
+{
+	int a = 1;
+	void do1()
+	{
+		a += 5;
+	}
+}
+
+namespace work1
+{
+	namespace work11 {
+		namespace work111 {
+			int a = 1;
+			void do1() 
+			{
+				a += 3;
+			}
+		}
+	}
+}
+
+// c++17 이상 가능
+namespace work2::work22::work222
+{
+	int a = 1;
+	void do1() 
+	{
+		a += 5;
+	}
+}
+
+int main()
+{
+	int apple = 5;
+
+	apple = 1;
+
+	{
+		int apple = 2;
+		cout << apple << endl;
+	}
+
+	cout << apple << endl;
+
+	work1::a;
+	work1::do1();
+
+	work2::a;
+	work2::do1();
+
+	work2::work22::work222::a;
+	work2::work22::work222::do1();
+
+	return 0;
+}
