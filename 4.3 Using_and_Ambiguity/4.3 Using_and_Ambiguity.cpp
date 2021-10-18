4ea346329107583ac5e5f3eec7797f1144695582
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-10-18 23:26:52 +0900

    4.3 Using 문과 모호성

---
+++ 4.3 Using_and_Ambiguity/4.3 Using_and_Ambiguity.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+namespace a
+{
+	int my_var(20);
+	int my_a(10);
+}
+
+namespace b
+{
+	int my_var(20);
+	int my_b(10);
+}
+
+int main()
+{
+	 using namespace std;
+	//using std::cout;
+	//using std::endl;
+
+	cout << "Hello" << endl;
+	cout << endl;
+
+	using namespace a;
+	using namespace b;
+
+	// ambiguous(모호성)
+	//cout << my_var << endl;
+
+	cout << my_a << endl;
+	cout << my_b << endl;
+
+	return 0;
+}
