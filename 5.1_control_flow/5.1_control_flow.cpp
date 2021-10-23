0a0aa10bc2dc5edae457638910f080c1b6a6e913
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-10-23 13:10:17 +0900

    5.1 제어 흐름 개요 Control flow

---
+++ 5.1_control_flow/5.1_control_flow.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cstdlib>	// exit()
+
+int main()
+{
+	/*
+		제어 흐름 (control flow)
+
+		1. 중단 (Halt)
+		2. 점프 (Jump) - goto, break, continue
+		3. 조건 분기 (Conditional branches) - if, switch
+		4. 반복 (Loops) - while, do while, for
+		5. 예외 처리 (Exceptions) - try, catch, throw
+	*/
+
+	std::cout << "I love you" << std::endl;
+
+	// 강제 종료 - debuging할 때 많이 사용
+	exit(0);
+
+	std::cout << "I love you, too" << std::endl;
+
+	return 0;
+}
