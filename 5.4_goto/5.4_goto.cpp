a55205d55841fbdd9c16694b491035ecbfa22e0a
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-10-24 13:04:44 +0900

    5.4 goto

---
+++ 5.4_goto/5.4_goto.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <cmath>	// sqrt()
+
+#define endl '\n'
+using namespace std;
+
+int main()
+{
+	/* goto 문 - 현재는 잘 안씀 */
+	double x;
+
+tryAgain:	//label
+
+	cout << "Enter a non-negative number" << endl;
+	cin >> x;
+
+	if (x < 0.0)
+		goto tryAgain;	// tryAgain으로 가라
+
+	cout << sqrt(x) << endl;
+
+	return 0;
+}
