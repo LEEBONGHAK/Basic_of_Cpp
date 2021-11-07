a7ca39d62579cbde70d0377b45df5a0350ecbac9
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-11-07 16:03:26 +0900

    6.2 배열 기초 [2 of 2] array

---
+++ 6.2_basic_of_array_2/6.2_basic_of_array_2.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+
+#define endl '\n'
+using namespace std;
+
+void doSomething(int my_arr[])
+{
+	cout << my_arr[0] << endl;
+	cout << my_arr[1] << endl;
+	cout << my_arr[2] << endl;
+	cout << (int)&my_arr[0] << endl;
+	cout << (int)&my_arr[1] << endl;
+	cout << (int)&my_arr[2] << endl;
+
+	cout << sizeof(my_arr) << endl;
+}
+
+int main()
+{
+	int students_scores[20];
+
+	cout << (int)&students_scores << endl;
+	cout << (int)&students_scores[0] << endl;
+	cout << (int)&students_scores[1] << endl;
+	cout << (int)&students_scores[2] << endl;
+	cout << (int)&students_scores[3] << endl;
+
+	cout << sizeof(students_scores) << endl;
+	cout << endl;
+
+	int my_arr[3] = { 1,2,3 };
+	cout << my_arr[0] << endl;
+	cout << my_arr[1] << endl;
+	cout << my_arr[2] << endl;
+	cout << (int)&my_arr[0] << endl;
+	cout << (int)&my_arr[1] << endl;
+	cout << (int)&my_arr[2] << endl;
+	cout << sizeof(my_arr) << endl;
+	
+	doSomething(my_arr);
+
+	return 0;
+}
