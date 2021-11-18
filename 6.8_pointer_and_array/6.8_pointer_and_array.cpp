36b2d7a9db1342930dab74b65b388f5b19d0ab5f
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-11-18 14:14:37 +0900

    6.8 포인터와 정적 배열

---
+++ 6.8_pointer_and_array/6.8_pointer_and_array.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+
+#define endl '\n'
+using namespace std;
+
+struct MyStruct
+{
+	int arr[5] = { 9,7,5,3,1, };
+};
+
+void doSometing(MyStruct ms)
+{
+	cout << sizeof(ms.arr) << endl;
+}
+
+void doSometing1(MyStruct *ms)
+{
+	cout << sizeof((*ms).arr) << endl;
+}
+
+void printArray(int arr[]) 
+{
+	cout << sizeof(arr) << endl;
+	cout << *arr << endl;
+	
+	*arr = 100;
+}
+
+int main()
+{
+	int arr[5] = { 9,7,5,3,1, };
+
+	cout << arr << endl;	// same with arr[0]'s address
+	cout << &(arr[0]) << endl;
+	
+	cout << *arr << endl;
+	cout << arr[0] << endl;
+	cout << endl;
+
+	int *ptr = arr;
+	cout << ptr << endl;
+	cout << *ptr << endl;
+	cout << endl;
+
+	cout << sizeof(arr) << endl;	// 5 X 4 = 20 bytes
+	cout << sizeof(ptr) << endl;	// 주소가 차지하는 메모리 공간
+	cout << endl;
+
+	printArray(arr);
+	cout << arr[0] << " " << *arr << endl;
+	cout << endl;
+
+	*ptr = 9;
+	cout << *ptr << " " << *(ptr + 1) << endl;
+	cout << endl;
+
+	char name[] = "jackjack";
+	cout << *name << endl;
+	cout << endl;
+
+	MyStruct ms;
+	cout << ms.arr[0] << endl;
+	cout << sizeof(ms.arr) << endl;
+	doSometing(ms);
+	doSometing1(&ms);
+
+	return 0;
+}
