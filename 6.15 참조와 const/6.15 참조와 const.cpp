5d511d10268479ba6b7e4bde2c0e0bb41e3e1644
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-11-22 22:18:09 +0900

    6.15 참조와 const

---
+++ 6.15 참조와 const/6.15 참조와 const.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+#define endl '\n'
+using namespace std;
+
+void doSomething(const int &x)
+{
+	std::cout << x << endl;
+}
+
+int main()
+{
+	int a = 1;
+	doSomething(a);
+	doSomething(1);
+	doSomething(a + 3);
+	doSomething(3 * 4);
+
+	return 0;
+}
