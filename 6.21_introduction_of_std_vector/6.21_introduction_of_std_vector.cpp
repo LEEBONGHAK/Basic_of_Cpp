5fb53e257f148d3e907d61a5074f6c268fd59071
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-12-05 17:25:49 +0900

    6.21 std::vector 소개

---
+++ 6.21_introduction_of_std_vector/6.21_introduction_of_std_vector.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+//#include <array>
+#include <vector>
+
+#define endl '\n'
+using namespace std;
+
+int main()
+{
+	//std::array<int, 5> arr;
+	std::vector<int> arr;	// vector는 알아서 메모리를 지워줌
+
+	std::vector<int> arr1 = { 1,2,3,4,5 };
+	cout << arr1.size() << endl;
+
+	std::vector<int> arr2 = { 1,2,3, };
+	cout << arr2.size() << endl;
+
+	std::vector<int> arr3 { 1,2,3, };
+	cout << arr3.size() << endl;
+
+
+	cout << endl;
+
+
+	vector<int> arr4 = { 1,2,3,4,5 };
+	for (auto &itr : arr4)
+		cout << itr << " ";
+	cout << endl;
+
+	cout << arr4.size() << endl;
+	cout << arr4[1] << endl;
+	cout << arr4.at(1) << endl;
+	cout << endl;
+
+	arr4.resize(10);	// vector size 변화
+	cout << arr4.size() << endl;
+	for (auto& itr : arr4)
+		cout << itr << " ";
+	cout << endl << endl;
+
+	arr4.resize(2);
+	cout << arr4.size() << endl;
+	for (auto& itr : arr4)
+		cout << itr << " ";
+	cout << endl;
+
+	return 0;
+}
