864a11409a020cffdda51573e6750804867b3c44
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-12-06 15:27:19 +0900

    7.1 매개변수와 실인자의 구분

---
+++ 7.1_divide_parameter_and_argument/7.1_divide_parameter_and_argument.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+#define endl '\n'
+using namespace std;
+
+int foo(int x, int y);	// int x, int y : parameters - 매개변수
+
+int foo(int x, int y)
+{
+	return x + y;
+}	// x and y are destoryed here
+
+int main()
+{
+	int x = 1, y = 2;
+
+	foo(6, 7);	// 6, 7 : arguments (actual parameters) - 실인자
+	foo(x, y + 1);
+
+	return 0;
+}
