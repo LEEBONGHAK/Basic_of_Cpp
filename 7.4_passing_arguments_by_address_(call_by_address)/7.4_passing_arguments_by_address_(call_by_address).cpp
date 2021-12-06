e3dbfc45bb462875c0d2071daac9249d839e8512
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-12-06 16:10:11 +0900

    7.4 주소에 의한 인수 전달

---
+++ 7.4_passing_arguments_by_address_(call_by_address)/7.4_passing_arguments_by_address_(call_by_address).cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+#define endl '\n'
+using namespace std;
+
+void foo(int *ptr)
+{
+	cout << *ptr << " " << ptr << " " << &ptr << endl;
+}
+
+int main()
+{
+	/* 주소에 의한 인수 전달 (pointer 사용) - Passing Arguments by Address (Call by Address) */
+
+	int value = 5;
+	cout << value << " " << &value << endl;
+	int* ptr = &value;
+	cout << &ptr << endl;
+	foo(ptr);
+	foo(&value);
+	//foo(5);	// error (literal이기 때문에 주소가 없음)
+
+	return 0;
+}
