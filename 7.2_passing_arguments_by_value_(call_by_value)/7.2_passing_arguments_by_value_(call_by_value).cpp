ff26965d02d9074f0763dcd21dd9b3a0c76ecbcc
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-12-06 15:33:52 +0900

    7.2 값에 의한 전달

---
+++ 7.2_passing_arguments_by_value_(call_by_value)/7.2_passing_arguments_by_value_(call_by_value).cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+#define endl '\n'
+using namespace std;
+
+void doSomething(int y)
+{
+	cout << "In func " << y << " " << &y << endl;
+}
+
+int main()
+{
+	doSomething(5);
+	cout << endl;
+
+	int x = 6;
+	cout << "In main " << x << " " << &x << endl;
+
+	doSomething(x);	// 실제로 전달되는 것을 x가 아닌 x의 값 '6'이 전달됨
+	doSomething(x + 1);
+
+	return 0;
+}
