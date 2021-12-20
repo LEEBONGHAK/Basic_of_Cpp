d59677d8ee9a42f5171a7144d92918c53990bdd8
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-12-20 14:07:25 +0900

    7.12 재귀적 함수 호출

---
+++ 7.12_recursive_function_call/7.12_recursive_function_call.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+#define endl '\n'
+using namespace std;
+
+void countDown(int count)
+{
+	cout << count << endl;
+	
+	if (count > 0) countDown(count - 1);
+}
+
+int sumTo(int sumto)
+{
+	if (sumto <= 0) return 0;
+	else if (sumto == 1) return 1;
+	else return sumTo(sumto - 1) + sumto;
+}
+
+int fibonacci(int n)
+{
+	if (n == 0 || n == 1) return 1;
+	else return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main()
+{
+	countDown(5);
+	cout << endl;
+	
+	cout << sumTo(10) << endl;
+	cout << endl;
+
+	for (int i = 0; i < 10; i++)
+		cout << fibonacci(i) << " ";
+	cout << endl;
+
+	return 0;
+}
