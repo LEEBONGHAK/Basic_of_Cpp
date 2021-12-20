b7d7311b7c2d5507f729dfb36a53bfd28c0c89d9
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-12-20 21:50:27 +0900

    7.15 명령줄 인수 command line arguments

---
+++ 7.15_command_line_arguments/7.15_command_line_arguments.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+
+#define endl '\n'
+using namespace std;
+
+/* 
+	명령줄 인수 - command line arguments 
+
+	- int argc : 개수
+	- char *argv[] : 실제 내용
+*/
+int main(int argc, char *argv[])
+{
+	for (int count = 0; count < argc; count++)
+		cout << argv[count] << endl;
+	cout << endl;
+
+	for (int count = 0; count < argc; count++)
+	{
+		string argv_single = argv[count];
+
+		if (count == 1)
+		{
+			int input_number = std::stoi(argv_single);		// stoi() : string to int (문자열을 정수형으로 변환)
+			cout << input_number + 1 << endl;
+		}
+		else 
+			cout << argv_single << endl;
+	}
+	cout << endl;
+
+	return 0;
+}
