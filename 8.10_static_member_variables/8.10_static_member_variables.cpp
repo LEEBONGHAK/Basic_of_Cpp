79245f80c3f810a566bea80ed72501d653310a87
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2022-02-07 14:50:24 +0900

    8.10 정적 멤버 변수

---
+++ 8.10_static_member_variables/8.10_static_member_variables.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+#define endl '\n'
+using namespace std;
+
+int generateID()
+{
+	static int s_id = 0;
+	return ++s_id;
+}
+
+class Something
+{
+public:
+	static int s_value;	// static member variable은 바로 초기화 불가능
+};
+
+int Something::s_value = 1;	// static의 경우 define in cpp (not in header file)
+
+int main()
+{
+	for (int i = 0; i < 4; i++)
+		cout << generateID() << " ";
+	cout << endl << endl;
+
+	cout << &Something::s_value << " " << Something::s_value << endl;
+
+	Something st1;
+	Something st2;
+
+	st1.s_value = 2;
+	cout << &st1.s_value << " " << st1.s_value << endl;
+	cout << &st2.s_value << " " << st2.s_value << endl;
+
+	Something::s_value = 1024;
+	cout << &Something::s_value << " " << Something::s_value << endl;
+
+	return 0;
+}
