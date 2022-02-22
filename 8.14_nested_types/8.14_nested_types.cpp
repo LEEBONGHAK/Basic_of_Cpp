4a7827b889fbd9fe93debe257454e0afa3dbd526
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2022-02-22 13:30:32 +0900

    8.14 클래스 안에 포함된 자료형 nested types

---
+++ 8.14_nested_types/8.14_nested_types.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+
+#define endl '\n'
+using namespace std;
+
+class Fruit
+{
+public:
+	// 특정 클래스만 사용하는 데이터 타입의 경우 밖에 두기보다 이렇게 사용하는 것도 괜찮음
+	enum FruitType
+	{
+		APPLE, BANANA, CHERRY,
+	};
+
+	class InnerClass
+	{
+
+	};
+
+	struct InnerStruct
+	{
+
+	};
+
+private:
+	FruitType m_type;
+
+public:
+	Fruit(FruitType type) : m_type(type)
+	{}
+
+	FruitType getType() { return m_type; }
+};
+
+
+
+
+int main()
+{
+	Fruit apple(Fruit::APPLE);
+
+	if (apple.getType() == Fruit::APPLE)
+	{
+		cout << "Apple" << endl;
+	}
+
+	return 0;
+}
