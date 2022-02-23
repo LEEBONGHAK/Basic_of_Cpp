ffba8745d5441017354cfc7c7c94a95c5876cd2f
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2022-02-23 15:44:18 +0900

    9.3 단항 연산자 오버로딩 하기

---
+++ 9.3_unary_operator_overloading/9.3_unary_operator_overloading.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <fstream>  // 파일 입출력
+
+#define endl '\n'
+using namespace std;
+
+class Cents
+{
+private:
+    int m_cents;
+
+public:
+    Cents(int cents = 0) { m_cents = cents; }
+    int getCents() const { return m_cents; }
+    int& getCents() { return m_cents; }
+
+    Cents operator - () const
+    {
+        return Cents(-m_cents);
+    }
+
+    bool operator ! () const
+    {
+        // m_cents == 0 -> return true;
+        return (m_cents == 0) ? true : false;
+    }
+
+    friend std::ostream& operator << (std::ostream& out, const Cents& cents)
+    {
+        out << cents.m_cents;
+        return out;
+    }
+
+    friend std::istream& operator >> (std::istream& in, Cents& cents)
+    {
+        in >> cents.m_cents;
+        return in;
+    }
+};
+
+int main()
+{
+    //int a = 3;
+    //cout << +a << endl;
+    //cout << -a << endl;
+    //cout << !a << endl;
+
+    Cents c1(6);
+    Cents c2(0);
+
+    // - operator
+    cout << c1 << endl;
+    cout << -c1 << endl;
+    cout << -Cents(-10) << endl << endl;
+
+    // not operator
+    cout << !c1 << " " << !c2 << endl << endl;
+
+    return 0;
+}
