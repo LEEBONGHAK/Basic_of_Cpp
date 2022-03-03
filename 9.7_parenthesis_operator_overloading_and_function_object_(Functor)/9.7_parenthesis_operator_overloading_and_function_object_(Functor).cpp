e15088cd19cfd5dc635e3742446176733a1959ab
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2022-03-03 12:28:24 +0900

    9.7 괄호 연산자 오버로딩과 함수 객체

---
+++ 9.7_parenthesis_operator_overloading_and_function_object_(Functor)/9.7_parenthesis_operator_overloading_and_function_object_(Functor).cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+#define endl '\n'
+using namespace std;
+
+class Accumulator
+{
+private:
+	int m_counter = 0;
+	
+public:
+	// parenthesis operator = ()
+	int operator () (int i)
+	{
+		return (m_counter += i);
+	}
+};
+
+int main()
+{
+	Accumulator acc;
+	// Functor
+	cout << acc(10) << endl;
+	cout << acc(20) << endl;
+
+	return 0;
+}
